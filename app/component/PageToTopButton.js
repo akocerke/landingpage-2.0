a40aab6ca9b302446de5ae74cc2540d49cc0973c
akocerke <wickedbitcoin@protonmail.com>
2024-11-17 08:31:15 +0100

    update

---
+++ app/component/PageToTopButton.js
@@ -0,0 +1,44 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { FaArrowUp } from "react-icons/fa"; // Verwenden des React-Icons
+
+export default function PageToTopButton() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Überwacht das Scrollen und zeigt den Button, wenn der Benutzer nach unten scrollt
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setIsVisible(true); // Button anzeigen, wenn mehr als 300px gescrollt
+      } else {
+        setIsVisible(false); // Button ausblenden, wenn weniger als 300px gescrollt
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  // Funktion, um zum Seitenanfang zu scrollen
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // sanftes Scrollen
+    });
+  };
+
+  return (
+    <button
+      onClick={scrollToTop}
+      className={`fixed bottom-6 right-6 p-4 rounded-full bg-purple-400 bg-opacity-35 text-white shadow-lg transform 
+       transition-all duration-500 ease-in-out hover:scale-110`}
+      
+    >
+      <FaArrowUp size={24} />
+    </button>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 "use client";
 import Hero from "./component/Hero";
 import Features from "./features/page";
+import PageToTopButton from "./component/PageToTopButton";
 
 export default function Home() {
   return (
     <>
+    <PageToTopButton/>
     <Hero />
     <Features/>
     
